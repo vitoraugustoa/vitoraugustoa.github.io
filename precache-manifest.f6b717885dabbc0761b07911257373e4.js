99630a8d3c5de7b896ea98058abb055d1178b4a0
Vitor Lopes <vitor.lopes@bhs.com.br>
2019-09-27 08:56:56 -0300

    Primeiro commit

---
+++ precache-manifest.f6b717885dabbc0761b07911257373e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e93e6f01474bc3c27e6edbae65863b6c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db67f91501bb98ba1a8a",
+    "url": "/static/css/main.6159bab1.chunk.css"
+  },
+  {
+    "revision": "85b5c4a038de30420353",
+    "url": "/static/js/2.57a463d0.chunk.js"
+  },
+  {
+    "revision": "db67f91501bb98ba1a8a",
+    "url": "/static/js/main.750ca7dc.chunk.js"
+  },
+  {
+    "revision": "0d7b8c3c0b9988eb9000",
+    "url": "/static/js/runtime-main.ea80b101.js"
+  },
+  {
+    "revision": "95a82eee3f0acdab9ae5500047f0ef38",
+    "url": "/static/media/logo.95a82eee.png"
+  }
+]);
